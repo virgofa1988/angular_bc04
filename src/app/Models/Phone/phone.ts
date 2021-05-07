592f8c700184a252c1c7ea604e8296615db8d4ea
virgofa1988 <nguyenanhtuan991988@gmail.com>
2021-05-07 11:44:16 +0700

    baitapwebbanhang

---
+++ src/app/Models/Phone/phone.ts
@@ -0,0 +1,9 @@
+export class Phone {
+        id: number;
+        name: string;
+        image: string;
+        description: string;
+        price: number;
+        inventory:  number;
+        rating:  number;
+}
