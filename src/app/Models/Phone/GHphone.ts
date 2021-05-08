a948be90c0497c60dbd9598f8d58f99ac66a8bfc
virgofa1988 <nguyenanhtuan991988@gmail.com>
2021-05-08 14:26:37 +0700

    Update Baitapwebbanhangangular

---
+++ src/app/Models/Phone/GHphone.ts
@@ -0,0 +1,10 @@
+export class GHphone {
+    id: number;
+    name: string;
+    image: string;
+    description: string;
+    price: number;
+    inventory:  number;
+    rating:  number;
+    quantity:number;
+}
--- src/app/baitapwebbanhang/baitapwebbanhang.component.ts
+++ src/app/baitapwebbanhang/baitapwebbanhang.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GHphone } from '../Models/Phone/GHphone';
 import {Phone} from '../Models/Phone/phone'
 @Component({
   selector: 'app-baitapwebbanhang',
@@ -35,9 +36,75 @@ export class BaitapwebbanhangComponent implements OnInit {
         rating: 4
     }
 ];
+  gioHang:GHphone[] =[]
+  tongTien:number = 0;
   constructor() { }
 
-  ngOnInit(): void {
+
+  onSelectedItem(item:Phone){
+  //   let GHitem =  {
+  //     id: item.id,
+  //     name: item.name,
+  //     image: item.image,
+  //     description: item.description,
+  //     price:item.price,
+  //     inventory:  item.inventory,
+  //     rating:  item.rating,
+  //     quantity:1,
+  // }; ==>ES6 có thể thay thế bằng 1 dòng này
+    let GHitem = {...item,quantity:1}
+
+    let index = this.gioHang.findIndex(sp => sp.id ===item.id )
+    if(index === -1){
+      this.gioHang.push(GHitem)      
+    } else {
+      this.gioHang[index].quantity +=1
+          }
+          this.tongTien = this.tinhTongTien()
+    // this.tongTien = this.gioHang.reduce((tong,sp,index)=>{
+    //   return tong += sp.price *sp.quantity
+    // },0)
+    
+    console.log("Gio Hang", this.gioHang)
+    console.log("tong Tien",this.tongTien)
+  }
+  tangGiamSL(id:number,sl:number){
+    // console.log(id,sl)
+    let index = this.gioHang.findIndex(sp => sp.id ===id);
+    // console.log(index)
+    if (index!==-1 ){
+      this.gioHang[index].quantity += sl
+    } 
+    if(this.gioHang[index].quantity < 1){
+      this.gioHang[index].quantity -= sl
+    }
+    this.tongTien = this.tinhTongTien()
+    // this.tongTien = this.gioHang.reduce((tong,sp,index)=>{
+    //   return tong += sp.price *sp.quantity
+    // },0)
+  }
+  deleteItem(id:number){
+    this.gioHang = this.gioHang.filter(sp =>sp.id !== id)
+    this.tongTien = this.tinhTongTien()
+    // this.tongTien = this.gioHang.reduce((tong,sp,index)=>{
+    //   return tong += sp.price *sp.quantity
+    // },0)
+  }
+  tinhTongTien(){
+    let Total = this.gioHang.reduce((tong,sp,index)=>{
+      return tong += sp.price *sp.quantity
+    },0)
+    return Total
+  }
+  onThanhToan(){
+    localStorage.setItem("gioHang",JSON.stringify(this.gioHang))
+    this.gioHang = []
+    this.tongTien=this.tinhTongTien()
+  }
+  //lifecycle
+  ngOnInit(): void {  
+    this.gioHang = JSON.parse(localStorage.getItem("gioHang") )
+    this.tongTien = this.tinhTongTien()
   }
 
 }
